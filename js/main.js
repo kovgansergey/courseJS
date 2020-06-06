cb86f4bcd0b31e3c966c67e4b5d661eb05f034c1
kovgansergey <skydiver.88@mail.ru>
2020-06-06 22:53:26 +0700

    lesson 2 done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ let addExpenses;
 let deposit;
 let mission;
 let period;
+let budgetDay;
 
-alert('Мое первое сообщение!');
-console.log('Сообщение в консоль');
+money = 31000;
+income = 'Парашютные прыжки';
+addExpenses = 'Транспорт на аэродром, Интернет, Подгузники ребенку, Ремонт машины';
+deposit = true;
+mission = 128000;
+period = 5;
+budgetDay = money / 30;
+
+console.log('type "money": ', typeof money);
+console.log('type "income": ', typeof income);
+console.log('type "deposit": ', typeof deposit);
+console.log('length "addExpenses": ', addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(','));
+console.log('budgetDay: ', budgetDay);
