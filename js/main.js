7df001e8d23ee5069754d6b6192cc235de79e2cd
kovgansergey <skydiver.88@mail.ru>
2020-06-16 21:39:54 +0700

    закончил основоное задание 9

--- js/main.js
+++ js/main.js
@@ -16,6 +16,14 @@ let additionalIncomeValue = document.getElementsByClassName('additional_income-v
 let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
 let incomePeriodValue = document.getElementsByClassName('income_period-value');
 let targetMonthValue = document.getElementsByClassName('target_month-value');
+let salaryAmount = document.querySelector('.salary-amount');
+let incomeTitle = document.querySelector('.income-title');
+let incomeAmount = document.querySelector('.income-amount');
+let expensesTitle = document.querySelector('.expenses-title');
+let expensesAmount = document.querySelector('.expenses-amount');
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+let targetAmount = document.querySelector('.target-amount');
+let periodSelect = document.querySelector('[type="range"]');
 
 let money;
 
