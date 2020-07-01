88850ad04f0eb66911def291d6d0473e4bb005d4
kovgansergey <skydiver.88@mail.ru>
2020-07-01 18:38:02 +0700

    обязательное задание 19. меню, модальное окно и анимация модального окна

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line strict
   'use strict';
 
-  // Timer
+  // Таймер
   function countTimer(deadline) {
     const timerHours = document.querySelector('#timer-hours');
     const timerMinutes = document.querySelector('#timer-minutes');
@@ -27,9 +27,7 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     }
 
-    const timerId = setInterval(updateClock, 1000);
-
-    function updateClock() {
+    const timerId = setInterval(() => {
       const timer = getTimeRemaining();
 
       if (timer.timeRemaining < 0) {
@@ -40,9 +38,60 @@ window.addEventListener('DOMContentLoaded', () => {
       timerHours.textContent = timerNumber(timer.hours);
       timerMinutes.textContent = timerNumber(timer.minutes);
       timerSeconds.textContent = timerNumber(timer.seconds);
-    }
+    }, 1000);
+
   }
 
   countTimer('2 july 2020');
+
+  // меню
+  function toggleMenu() {
+    const menuBtn = document.querySelector('.menu');
+    const menu = document.querySelector('menu');
+    const menuCloseBtn = menu.querySelector('.close-btn');
+    const menuItems = menu.querySelectorAll('ul>li>a');
+
+    function handlerMenu() {
+      menu.classList.toggle('active-menu');
+    }
+
+    menuBtn.addEventListener('click', handlerMenu);
+    menuCloseBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach(item => item.addEventListener('click', handlerMenu));
+  }
+
+  toggleMenu();
+
+  // модальное окно
+  function togglePopup() {
+    const popup = document.querySelector('.popup');
+    const popupBtn = document.querySelectorAll('.popup-btn');
+    const popupCloseBtn = popup.querySelector('.popup-close');
+
+    function popupAnimate() {
+      let opacity = 0;
+      requestAnimationFrame(function popupAnim() {
+        popup.style.opacity = `${opacity}%`;
+
+        if (opacity <= 100) {
+          opacity++;
+          requestAnimationFrame(popupAnim);
+        }
+      });
+    }
+
+    popupBtn.forEach(item => item.addEventListener('click', () => {
+      popup.style.display = 'block';
+      if (window.innerWidth >= 768) {
+        popupAnimate();
+      }
+    }));
+
+    popupCloseBtn.addEventListener('click', () => {
+      popup.style.display = '';
+    });
+  }
+
+  togglePopup();
 
 });
