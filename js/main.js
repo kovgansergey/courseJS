88abb3774d998ccf7a31bb3ca814466900d85e04
Ковган Сергей Владимирович <Kovgan_SV@Gate.net>
2020-07-02 16:24:46 +0700

    усложненное 20. все меню в один обработчик события, закрытие меню при клике

--- js/main.js
+++ js/main.js
@@ -46,14 +46,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // меню
   function toggleMenu() {
-    const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
-    const mainLink = document.querySelector('main>a');
 
-    function handlerMenu() {
-      menu.classList.toggle('active-menu');
-    }
-
+    // функция плавного скролла ссылок меню
     function scrollLink(item) {
 
       const blockId = item.getAttribute('href').substr(1);
@@ -64,24 +59,33 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
 
-    menuBtn.addEventListener('click', handlerMenu);
+    // одно событие на весь документ
+    document.addEventListener('click', event => {
+      let target = event.target;
 
-    menu.addEventListener('click', event => {
-      const target = event.target;
+      // если меню активировано - закрытие при клике мимо меню
+      if (menu.classList.contains('active-menu') && !target.closest('menu')) {
+        menu.classList.remove('active-menu');
+      }
 
-      if (target.tagName === 'A') {
-        event.preventDefault();
-        handlerMenu();
+      // открытие меню при клике на кнопку меню
+      if (target.closest('.menu')) {
+        menu.classList.add('active-menu');
+      }
 
-        if (!target.classList.contains('close-btn')) {
-          scrollLink(target);
+      // закрытие меню и скролл ссылок в меню и на главной секции
+      if (target.closest('main') || target.closest('menu')) {
+        target = target.closest('a');
+
+        if (target) {
+          event.preventDefault();
+          menu.classList.remove('active-menu');
+
+          if (!target.classList.contains('close-btn')) {
+            scrollLink(target);
+          }
         }
       }
-    });
-
-    mainLink.addEventListener('click', () => {
-      event.preventDefault();
-      scrollLink(event.target.parentNode);
     });
   }
 
